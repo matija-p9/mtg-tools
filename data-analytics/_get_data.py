d0f3f8f3f44f7d03218977b9c2077f7ffcf039d3
matija-p9 <59355738+matija-p9@users.noreply.github.com>
2024-05-16 12:54:59 +0200

    Add files via upload

--- data-analytics/_get_data.py
+++ data-analytics/_get_data.py
@@ -1,11 +1,20 @@
-tournament = "2024-04-24"
 import calculate as calc
 import openpyxl
-wb = openpyxl.load_workbook("./reference/liga2.xlsx")
-ws = wb[tournament]
+
+excel_file = openpyxl.load_workbook("./reference/liga2.xlsx")
+date_range = ["2024-05-08", "2024-05-08"]
+
+# Get metagame split per deck.
+decks, meta_split = calc.metagame(excel_file, date_range[0], date_range[1])
+# Print metagame split per deck.
+print()
+print("METAGAME SPLIT")
+print("--------------")
+for key in meta_split:
+    print(f"{key}: {meta_split[key]:.2f}")
 
 # Get winrate per player.
-winners, losers = calc.player_wr(ws)
+winners, losers = calc.player_wr(excel_file, date_range[0], date_range[1])
 winrate = calc.winrate(winners, losers)
 # Print winrate per player.
 print()
@@ -14,10 +23,8 @@ print("---------------")
 for key in winrate:
     print(f"{key}: {winrate[key]:.2f}")
 
-#######################################
-
 # Get winrate per deck.
-winners, losers = calc.deck_wr(ws)
+winners, losers = calc.deck_wr(excel_file, date_range[0], date_range[1])
 winrate = calc.winrate(winners, losers)
 # Print winrate per deck.
 print()
@@ -25,14 +32,3 @@ print("DECK WINRATES")
 print("-------------")
 for key in winrate:
     print(f"{key}: {winrate[key]:.2f}")
-
-#######################################
-
-# Get metagame split per deck.
-decks, meta_split = calc.metagame(wb["Sheet1"], tournament)
-# Print metagame split per deck.
-print()
-print("METAGAME SPLIT")
-print("--------------")
-for key in meta_split:
-    print(f"{key}: {meta_split[key]:.2f}")
--- data-analytics/calculate.py
+++ data-analytics/calculate.py
@@ -1,21 +1,36 @@
 ################################################################
 # Calculate and return list of decks and meta presence per deck.
-def metagame(excel_worksheet, tournament_date):
+def metagame(excel_file, t_date_1, t_date_2):
+    ws = excel_file["Sheet1"]
 
-    TOURNAMENT_SIZE = 50    # Arbitrary upper limit for purposes of searching.
+    # Arbitrary upper limit for search purposes.
+    TOURNAMENT_SIZE = 50
+
+    # Prepare a list of tournaments.
+    tournaments = []
+    is_left_bound = False
+    for cell in ws[1]:
+        # Logic to check  area between two given dates.
+        if t_date_1 in str(cell.value): is_left_bound = True
+        if str(cell.value) != "None" and is_left_bound:
+            tournaments.append(str(cell.value)[:10])
+        if t_date_2 in str(cell.value): break
+
     decks = []
-    # Search the sheet for tournament_date.
-    for row in excel_worksheet:
+    i = 0   # Iterator for tournament reference.
+    # Search the sheet for tournaments.
+    for row in ws:
         for cell in row:
-            if tournament_date in str(cell.value):
-                i = 0
-                while i < TOURNAMENT_SIZE:
+            if tournaments[i] in str(cell.value):
+                j = 0
+                while j < TOURNAMENT_SIZE:
                     # Add non-empty cells from that date to decks list.
-                    if excel_worksheet.cell(row=cell.row+2+i,
-                                            column=cell.column+2).value:
-                        decks.append(excel_worksheet.cell(row=cell.row+2+i,
-                                                          column=cell.column+2).value)
-                    i += 1
+                    if ws.cell(row=cell.row+2+j,
+                               column=cell.column+2).value:
+                        decks.append(ws.cell(row=cell.row+2+j,
+                                             column=cell.column+2).value)
+                    j += 1
+                if i < len(tournaments)-1: i += 1
 
     # Convert decks into a Counter.
     from collections import Counter
@@ -23,71 +38,101 @@ def metagame(excel_worksheet, tournament_date):
     # Calculate and assign meta presence % to each deck.
     for deck in decks_count:
         decks_count[deck] = float(decks_count[deck] / len(decks) * 100)
-    # Sort data in descending order.
-    meta_split = dict(sorted(decks_count.items(), key=lambda item:item[1], reverse=True))
+    # Sort data in descending, alphabetical order.
+    sort_dc_alpha = dict(sorted(decks_count.items()))
+    meta_split = dict(sorted(sort_dc_alpha.items(), key=lambda item:item[1], reverse=True))
 
     return decks, meta_split
 
 ##################################################################################
 # Separate winners and losers into lists, ignoring mirrors for decks. Draw = loss.
-def deck_wr(excel_worksheet):
+def deck_wr(excel_file, t_date_1, t_date_2):
+    ws = excel_file["Sheet1"]
 
+    # Prepare a list of tournaments.
+    tournaments = []
+    is_left_bound = False
+    for cell in ws[1]:
+        # Logic to check  area between two given dates.
+        if t_date_1 in str(cell.value): is_left_bound = True
+        if str(cell.value) != "None" and is_left_bound:
+            tournaments.append(str(cell.value)[:10])
+        if t_date_2 in str(cell.value): break
+
+    # Prepare empty lists to populate.
     winners = []
     losers = []
-    i = 1   # Iterator for rows reference.
-    for row in excel_worksheet:
-        # Skip ROUND separator rows.
-        if "ROUND" in str(excel_worksheet['A'+str(i)].value):
+    for date in tournaments:
+        # Assign the correctly dated sheet.
+        ws = excel_file[date]
+        i = 1   # Iterator for rows reference.
+        for row in ws:
+            # Skip ROUND separator rows.
+            if "ROUND" in str(ws['A'+str(i)].value):
+                i += 1
+                continue
+            # Compare deck names, if mirror match, skip row.
+            if ws['B'+str(i)].value == ws['E'+str(i)].value:
+                i += 1
+                continue
+            # Compare score, if equal (draw), both are losers.
+            if ws['C'+str(i)].value == ws['D'+str(i)].value:
+                losers.append(ws['B'+str(i)].value)
+                losers.append(ws['E'+str(i)].value)
+                i += 1
+                continue
+            # Check C and D columns for winner and loser, append.
+            if ws['C'+str(i)].value > ws['D'+str(i)].value:
+                winners.append(ws['B'+str(i)].value)
+                losers.append(ws['E'+str(i)].value)
+            else:
+                winners.append(ws['E'+str(i)].value)
+                losers.append(ws['B'+str(i)].value)
             i += 1
-            continue
-        # Compare deck names, if mirror match, skip row.
-        if excel_worksheet['B'+str(i)].value == excel_worksheet['E'+str(i)].value:
-            i += 1
-            continue
-        # Compare score, if equal (draw), both are losers.
-        if excel_worksheet['C'+str(i)].value == excel_worksheet['D'+str(i)].value:
-            losers.append(excel_worksheet['B'+str(i)].value)
-            losers.append(excel_worksheet['E'+str(i)].value)
-            i += 1
-            continue
-        # Check C and D columns for winner and loser, append.
-        if excel_worksheet['C'+str(i)].value > excel_worksheet['D'+str(i)].value:
-            winners.append(excel_worksheet['B'+str(i)].value)
-            losers.append(excel_worksheet['E'+str(i)].value)
-        else:
-            winners.append(excel_worksheet['E'+str(i)].value)
-            losers.append(excel_worksheet['B'+str(i)].value)
-        i += 1
 
     return winners, losers
 
 ######################################################
 # Separate winners and losers into lists. Draw = loss.
-def player_wr(excel_worksheet):
+def player_wr(excel_file, t_date_1, t_date_2):
+    ws = excel_file["Sheet1"]
+
+    # Prepare a list of tournaments.
+    tournaments = []
+    is_left_bound = False
+    for cell in ws[1]:
+        # Logic to check  area between two given dates.
+        if t_date_1 in str(cell.value): is_left_bound = True
+        if str(cell.value) != "None" and is_left_bound:
+            tournaments.append(str(cell.value)[:10])
+        if t_date_2 in str(cell.value): break
 
     # Prepare empty lists to populate.
     winners = []
     losers = []
-    i = 1   # Iterator for rows reference.
-    for row in excel_worksheet:
-        # Skip ROUND separator rows.
-        if "ROUND" in str(excel_worksheet['A'+str(i)].value):
+    for date in tournaments:
+        # Assign the correctly dated sheet.
+        ws = excel_file[date]
+        i = 1   # Iterator for rows reference.
+        for row in ws:
+            # Skip ROUND separator rows.
+            if "ROUND" in str(ws['A'+str(i)].value):
+                i += 1
+                continue
+            # Compare score, if equal (draw), both are losers.
+            if ws['C'+str(i)].value == ws['D'+str(i)].value:
+                losers.append(ws['A'+str(i)].value)
+                losers.append(ws['F'+str(i)].value)
+                i += 1
+                continue
+            # Check C and D columns for winner and loser, append.
+            if ws['C'+str(i)].value > ws['D'+str(i)].value:
+                winners.append(ws['A'+str(i)].value)
+                losers.append(ws['F'+str(i)].value)
+            else:
+                winners.append(ws['F'+str(i)].value)
+                losers.append(ws['A'+str(i)].value)
             i += 1
-            continue
-        # Compare score, if equal (draw), both are losers.
-        if excel_worksheet['C'+str(i)].value == excel_worksheet['D'+str(i)].value:
-            losers.append(excel_worksheet['A'+str(i)].value)
-            losers.append(excel_worksheet['F'+str(i)].value)
-            i += 1
-            continue
-        # Check C and D columns for winner and loser, append.
-        if excel_worksheet['C'+str(i)].value > excel_worksheet['D'+str(i)].value:
-            winners.append(excel_worksheet['A'+str(i)].value)
-            losers.append(excel_worksheet['F'+str(i)].value)
-        else:
-            winners.append(excel_worksheet['F'+str(i)].value)
-            losers.append(excel_worksheet['A'+str(i)].value)
-        i += 1
 
     return winners, losers
 
@@ -115,7 +160,9 @@ def winrate(winners, losers):
         # If winner has no losses, winrate = 100.
         if winner not in winrate:
             winrate[winner] = float(100)
-    # Sort winrate in descending order.
-    winrate = dict(sorted(winrate.items(), key=lambda item:item[1], reverse=True))
+
+    # Sort data in descending, alphabetical order.
+    sort_wr_alpha = dict(sorted(winrate.items()))
+    winrate = dict(sorted(sort_wr_alpha.items(), key=lambda item:item[1], reverse=True))
 
     return winrate
